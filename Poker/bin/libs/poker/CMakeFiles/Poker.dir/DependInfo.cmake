fddedaf5cbf10707036398679d608b257b866b7f
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-01-14 11:16:19 -0500

    rework on project structure: poker only

---
+++ Poker/bin/libs/poker/CMakeFiles/Poker.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kahshiuh/Desktop/kahshiuh/Mimic/Poker/libs/poker/src/Card.cpp" "libs/poker/CMakeFiles/Poker.dir/src/Card.cpp.o" "gcc" "libs/poker/CMakeFiles/Poker.dir/src/Card.cpp.o.d"
+  "/home/kahshiuh/Desktop/kahshiuh/Mimic/Poker/libs/poker/src/Dealer.cpp" "libs/poker/CMakeFiles/Poker.dir/src/Dealer.cpp.o" "gcc" "libs/poker/CMakeFiles/Poker.dir/src/Dealer.cpp.o.d"
+  "/home/kahshiuh/Desktop/kahshiuh/Mimic/Poker/libs/poker/src/Deck.cpp" "libs/poker/CMakeFiles/Poker.dir/src/Deck.cpp.o" "gcc" "libs/poker/CMakeFiles/Poker.dir/src/Deck.cpp.o.d"
+  "/home/kahshiuh/Desktop/kahshiuh/Mimic/Poker/libs/poker/src/Game.cpp" "libs/poker/CMakeFiles/Poker.dir/src/Game.cpp.o" "gcc" "libs/poker/CMakeFiles/Poker.dir/src/Game.cpp.o.d"
+  "/home/kahshiuh/Desktop/kahshiuh/Mimic/Poker/libs/poker/src/Player.cpp" "libs/poker/CMakeFiles/Poker.dir/src/Player.cpp.o" "gcc" "libs/poker/CMakeFiles/Poker.dir/src/Player.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
